8d293cd1b80945378bee9af86b3ec174a025d3cd
Lauren Wolfe <wolfela@uw.edu>
2017-10-19 21:36:40 -0700

    Ingesting bittrex api data successfully: currencies, markets, tickers, detailed market summary.
    Ready to test DB insertion.

--- altcoin-manager/main.py
+++ altcoin-manager/main.py
@@ -3,48 +3,48 @@ import configparser
 
 
 def enter_transaction():
-    print("Enter X to finish inputting data.")
     symbol = ''
     purchase_date = ''
     shares = ''
     unit_price = ''
 
-    while True:
-        while symbol is None:
-            symbol = input('Enter currency symbol:')
-            if check_for_exit(symbol):
-                return
-            if symbol is None:
-                print("Blank entries are not valid, please try again.")
-                continue
+    print("Enter X to finish inputting data and exit.")
 
-        while purchase_date is None:
-            purchase_date = input('Enter purchase date:')
-            if check_for_exit(purchase_date):
-                return
-            if purchase_date is None:
-                print("Blank entries are not valid, please try again.")
-                continue
+    while symbol == '':
+        symbol = input('Enter currency symbol:')
+        if check_for_exit(symbol):
+            return
+        if symbol == '':
+            print("Blank entries are not valid, please try again.")
+            continue
 
-        while shares is None:
-            shares = input('Enter number of coins purchased:')
-            if check_for_exit(shares):
-                return
-            if shares is None:
-                print("Blank entries are not valid, please try again.")
-                continue
+    while purchase_date == '':
+        purchase_date = input('Enter purchase date:')
+        if check_for_exit(purchase_date):
+            return
+        if purchase_date == '':
+            print("Please enter the purchase date, or X to exit.")
+            continue
 
-        while unit_price is None:
-            unit_price = input('Enter coin unit price:')
-            if check_for_exit(unit_price):
-                return
-            if unit_price is None:
-                print("Blank entries are not valid, please try again.")
-                continue
+    while shares == '':
+        shares = input('Enter number of coins purchased:')
+        if check_for_exit(shares):
+            return
+        if shares == '':
+            print("Please enter the number of coins purchased, or X to exit.")
+            continue
 
-        data = [symbol, purchase_date, shares, unit_price]
-        query = "INSERT INTO transactions (symbol, purchase_date, num_units, unit_price) VALUES (%s, %s, %s, %s)"
-        insert_data_into_db(data, query)
+    while unit_price == '':
+        unit_price = input('Enter coin unit price:')
+        if check_for_exit(unit_price):
+            return
+        if unit_price == '':
+            print("Please enter the per-coin price, or X to exit.")
+            continue
+
+    data = [symbol, purchase_date, shares, unit_price]
+    query = "INSERT INTO transactions (symbol, purchase_date, num_units, unit_price) VALUES (%s, %s, %s, %s)"
+    insert_data_into_db(data, query)
 
 
 def insert_data_into_db(data, query):
